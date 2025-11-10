e0df60287008422443906dc14418f9caf36325d3
MickeyMoussa <s202349310@kfupm.edu.sa>
2025-11-10 09:41:36 +0300

    Update server.js

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,20 +2,20 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
-
-
-{/*write code to define routes*/}
-
-
-{/*write code to create server*/}
+const app = express();
+const PORT = 3000;
 
 app.use(cors())
 
 app.get("/api/quote", (req, res) => {
-      res.json(getRandomQuote());
+      const randomQuote = getRandomQuote();
+      res.json({ quote: randomQuote });
     });
 
 app.get("/", (req, res) => {
       res.send("Welcome to the Quote Generator API");
+    });
+
+app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
     });
